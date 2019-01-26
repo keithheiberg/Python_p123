f494f7f13a248a7cd09d6666d8e1f7705659a831
keithheiberg <46172311+keithheiberg@users.noreply.github.com>
2019-01-26 12:25:25 -0600

    Add files via upload

--- Python-Course/drillp123_phonebook_gui.py
+++ Python-Course/drillp123_phonebook_gui.py
@@ -10,18 +10,17 @@ def load_gui(self):
 	self.lbl_destDir = tk.Label(self.master,text='Destination Directory:')
 	self.lbl_destDir.grid(row=3,column=0,padx=(10,0),pady=(10,0),sticky=N+W)
 
-	self.txt_sourceDir = tk.Entry(self.master,text='Source directory goes here...',width=65)
+	self.txt_sourceDir = tk.Entry(self.master,width=65)
 	self.txt_sourceDir.grid(row=1,column=0,rowspan=1,columnspan=6,padx=(10,10),pady=(10,0),sticky=NSEW)
-	self.txt_destDir = tk.Entry(self.master,text='Destination directory goes here...',width=65)
+	self.txt_destDir = tk.Entry(self.master,width=65)
 	self.txt_destDir.grid(row=4,column=0,rowspan=1,columnspan=6,padx=(10,10),pady=(10,0),sticky=NSEW)
 
 	self.btn_addSource = tk.Button(self.master,width=18,height=2,text='Source Dir',command=lambda: drillp123_phonebook_func.onSelectSource(self))
 	self.btn_addSource.grid(row=5,column=0,padx=(10,0),pady=(15,10),sticky=W)
 	self.btn_addDest = tk.Button(self.master,width=18,height=2,text='Destination Dir',command=lambda: drillp123_phonebook_func.onSelectDest(self))
 	self.btn_addDest.grid(row=5,column=2,padx=(10,0),pady=(15,10),sticky=W)
-	self.btn_addText = tk.Button(self.master,width=18,height=2,text='Add Text files',command=lambda: drillp123_phonebook_func.onSelect(self))
+	self.btn_addText = tk.Button(self.master,width=18,height=2,text='Move Text files',command=lambda: drillp123_phonebook_func.onSelectText(self))
 	self.btn_addText.grid(row=5,column=4,padx=(10,0),pady=(15,10),sticky=W)
 	
 	drillp123_phonebook_func.create_db(self)
-	drillp123_phonebook_func.onRefresh(self)
 
